2a23a8a90d48ae61e3fd3a9a40f3e645f70a30f5
Ankit Gupta <ag3831124@gmail.com>
2022-09-27 18:02:14 +0530

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { SocialAuthService, FacebookLoginProvider } from '@abacritt/angularx-social-login';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-facebook';
+  constructor(private authService: SocialAuthService) { }
+  user:any;
+  loggedIn:any;
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
+      this.loggedIn = (user != null);
+    });
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
 }
